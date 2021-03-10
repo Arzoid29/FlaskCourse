c739a17a7e698603af7a27f6882cb3b9ad84e070
Jose David <josedavidsanchez29@gmail.com>
2021-03-10 19:36:13 -0400

    Agregando session

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, redirect,make_response, render_template
+from flask import Flask, request, redirect,make_response, render_template,session
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 bootstrap =Bootstrap(app)
+
+app.config["SECRET_KEY"]= "SUPER SECRETO"
 
 
 todos = ["comprar cafe ", "solicitud de compra ", "Traer"]
@@ -17,13 +19,13 @@ def index():
     user_ip = request.remote_addr
     
     response = make_response(redirect('/hello'))
-    response.set_cookie("user_ip", user_ip)
+    session["user_ip"] = user_ip
    
     return response
 
 @app.route('/hello')
 def hello():
-    user_ip = request.cookies.get("user_ip")
+    user_ip = session.get("user_ip")
     context = { "user_ip":user_ip, "todos":todos,}
     
     return render_template("hello.html", **context)
